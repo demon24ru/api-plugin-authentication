9ec1ab2f64d09519c7cadfc3cebdedab0e077b25
demon-dem <ittek@inbox.ru>
2022-07-07 14:40:08 +0700

    init

--- src/config.js
+++ src/config.js
@@ -5,8 +5,6 @@ const { str, bool } = envalid;
 export default envalid.cleanEnv(
   process.env,
   {
-    STORE_URL: str({ devDefault: "http://localhost:4000" }),
-    TOKEN_SECRET: str({ default: "UPDATE_THIS_SECRET" }),
     OAUTH2_ADMIN_URL: str({ default: "http://s.clubnika.me:4445" }),
     MOCK_TLS_TERMINATION: bool({ default: false })
   },
