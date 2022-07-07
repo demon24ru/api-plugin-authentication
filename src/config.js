5918ade9d3612f03f54b82569027f72207fe21a9
demon-dem <ittek@inbox.ru>
2022-07-07 15:14:39 +0700

    init

--- src/config.js
+++ src/config.js
@@ -6,7 +6,12 @@ export default envalid.cleanEnv(
   process.env,
   {
     OAUTH2_ADMIN_URL: str({ default: "http://s.clubnika.me:4445" }),
-    MOCK_TLS_TERMINATION: bool({ default: false })
+    MOCK_TLS_TERMINATION: bool({ default: false }),
+    MONGO_URL: str({
+      devDefault: "mongodb://localhost:27017/reaction",
+      desc: "A valid MongoDB connection string URI, ending with the database name",
+      example: "mongodb://localhost:27017/reaction"
+    })
   },
   {
     dotEnvPath: null
